9e69be5c4777101541f483dffdbd35a3f9268dc8
Ashmeet Singh <ashmeetsingh987@gmail.com>
2021-07-30 15:01:01 +0530

    demo

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -4,7 +4,7 @@ import SidebarRow from './SidebarRow';
 import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
 import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags';
 import PeopleIcon from '@material-ui/icons/People';
-import ChatIcon from '@material-ui/icons/Chat';
+import ChatIcon from '@material-ui/icons/Chat'
 import StorefrontIcon from '@material-ui/icons/Storefront';
 import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
 import ExpandMoreOutlinedIcon from '@material-ui/icons/ExpandMoreOutlined';
